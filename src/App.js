d149d2fc76bcee6b3ca944f90692c7a810d0d9fc
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-10-14 11:27:14 -0600

    lecture1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Parent from './components/Parent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      input: '', 
+      pancakeURL: ''
+    }
+  }
+
+  handleChange(event){
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  showAPancake(){
+    this.setState({
+      pancakeURL: 'https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco/https%3A%2F%2Fstorage.googleapis.com%2Fgen-atmedia%2F3%2F2018%2F12%2F8cfbcbb2919742682345681d469b7417a73e4dfe.jpeg'
+    })
+  }
+
+  render(){
+    
+    return (
+      <div className="App">
+        <input onChange={(event) => this.handleChange(event)}/>
+        <h1>{this.state.input}</h1>
+        <button onClick={()=> this.showAPancake()}>Click for pancakes</button>
+        <br></br>
+        <img src={this.state.pancakeURL}/>
+        <hr/>
+        <Parent/>
+      </div>
+    );
+  }
 }
 
-export default App;
+// export default App;
